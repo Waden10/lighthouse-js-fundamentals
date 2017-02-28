b2778e78d86eaab6677d93dfe2726ad984c6342f
waden10 <fantm10@hotmail.com>
2017-02-28 07:24:35 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utiensils", "glasses", "cups", " cooking utensils"];
+
+console.log("Kitchen sutff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
