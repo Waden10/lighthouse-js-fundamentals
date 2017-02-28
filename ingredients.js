a2ccc3eab6d6cf7ee3a16904e05ae7b12ed1d3ae
waden10 <fantm10@hotmail.com>
2017-02-28 20:10:20 +0000

    ingredients list

---
+++ ingredients.js
@@ -0,0 +1,23 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("While ingredients");
+var i = 0;
+while (i < ingredients.length) {
+console.log(ingredients[i]);
+i++
+}
+
+console.log("For Ingredients");
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+console.log("Backwards");
+var reversed = ingredients.reverse()
+console.log(reversed);
+
+
+
+
+
+
